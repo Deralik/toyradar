c6fa0597bf6162c3ffcc4bc500283c093ba018e8
Deralik <deralik.ship@gmail.com>
2026-09-09 08:31:24 -0700

    demo setup

--- demos/a2_mti_and_doppler.py
+++ demos/a2_mti_and_doppler.py
@@ -8,3 +8,12 @@ import numpy as np
 IPIX_DIR   = "./data/ipix/" 
 RESULT_DIR = "./reports/"
 
+SNTH_PRF = 5000
+SNTH_WAV_L = 0.03
+SNTH_N_PULSE = 128
+SNTH_N_BINS = 64
+SNTH_CELL_W = 30
+SNTH_VEL_UA = SNTH_WAV_L * SNTH_PRF / 4
+SNTH_DV = 2 * SNTH_VEL_UA / SNTH_N_PULSE
+
+
